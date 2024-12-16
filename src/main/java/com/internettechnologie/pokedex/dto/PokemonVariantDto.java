64b7573b13356b678b10f2843aac8215aa4f4042
TuceYBLN <tuce_yemisen@hotmail.de>
2024-12-16 18:12:42 +0100

    Dto für Pokemonvariante hinzugefügt

---
+++ src/main/java/com/internettechnologie/pokedex/dto/PokemonVariantDto.java
@@ -0,0 +1,16 @@
+package com.internettechnologie.pokedex.dto;
+
+import java.util.List;
+
+public class PokemonVariantDto {
+
+    private List<String> pokemonVariante;
+
+    public List<String> getPokemonVariante() {
+        return pokemonVariante;
+    }
+
+    public void setPokemonVariante(List<String> pokemonVariante) {
+        this.pokemonVariante = pokemonVariante;
+    }
+}
