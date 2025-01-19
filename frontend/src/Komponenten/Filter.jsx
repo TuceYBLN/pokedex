75f910f067498ee1d61f899b84a369cce9807196
TuceYBLN <tuce_yemisen@hotmail.de>
2025-01-19 22:38:59 +0100

    css für Filter-Komponente und alle erforderlichen Bootstrap Komponenten hinzugefügt

--- frontend/src/Komponenten/Filter.jsx
+++ frontend/src/Komponenten/Filter.jsx
@@ -1,21 +1,31 @@
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
+import Form from "react-bootstrap/Form";
 import React from "react";
 
 function Filter() {
-
-
   return (
-      <Dropdown className="content-padding">
-        <Dropdown.Toggle id="dropdown-autoclose-true">
-          Region
-        </Dropdown.Toggle>
-
+    <div className="content-padding filter-container">
+      <Dropdown className="filter-component">
+        <Dropdown.Toggle id="dropdown-autoclose-true">Region</Dropdown.Toggle>
         <Dropdown.Menu>
           <Dropdown.Item>Kanto</Dropdown.Item>
           <Dropdown.Item>Johto</Dropdown.Item>
           <Dropdown.Item>Hoenn</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
+      <Dropdown className="filter-component">
+        <Dropdown.Toggle id="dropdown-autoclose-true">Types</Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item>Kanto</Dropdown.Item>
+          <Dropdown.Item>Johto</Dropdown.Item>
+          <Dropdown.Item>Hoenn</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Form>
+        <Form.Check label="Shiny" />
+        <Form.Check label="Owning" />
+      </Form>
+    </div>
   );
 }
 
