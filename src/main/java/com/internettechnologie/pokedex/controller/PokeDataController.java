d51f49cacc06a16bc53fe41512aa6e7e558ca3df
TuceYBLN <tuce_yemisen@hotmail.de>
2025-02-06 08:58:08 +0100

    docker aufgesetzt und url anpassungen

--- src/main/java/com/internettechnologie/pokedex/controller/PokeDataController.java
+++ src/main/java/com/internettechnologie/pokedex/controller/PokeDataController.java
@@ -3,18 +3,28 @@ package com.internettechnologie.pokedex.controller;
 import com.internettechnologie.pokedex.dto.PokeDataDto;
 import com.internettechnologie.pokedex.service.PokeDataService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-public class PokeDataController {
+public class PokeDataController implements WebMvcConfigurer {
 
     @Autowired
     private PokeDataService pokeDataService;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://curious-pika-1274df.netlify.app/",
+                        "http://localhost:3000"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 
     @GetMapping("/pokedata")
     public List<PokeDataDto> getData() {
--- src/main/java/com/internettechnologie/pokedex/controller/PokeVariantController.java
+++ src/main/java/com/internettechnologie/pokedex/controller/PokeVariantController.java
@@ -5,18 +5,27 @@ import com.internettechnologie.pokedex.entity.PokeVariant;
 import com.internettechnologie.pokedex.service.PokeVariantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
-public class PokeVariantController {
+public class PokeVariantController implements WebMvcConfigurer {
 
     @Autowired
     private PokeVariantService pokeOwnerService;
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://curious-pika-1274df.netlify.app/",
+                        "http://localhost:3000"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 
     @PostMapping("/owner")
     public PokeVariant save(@RequestBody PokeVariantOwnedDto id) throws HttpRequestMethodNotSupportedException {
